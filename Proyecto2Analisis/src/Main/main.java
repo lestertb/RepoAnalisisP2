8237596507c3d064d16098cd5b306cadf1cb030e
lester <lestertb@estudiantec.cr>
2020-08-03 01:35:18 -0600

    Test Funcional Nuevo Grafo
    Dijkstra

--- Proyecto2Analisis/src/Main/main.java
+++ Proyecto2Analisis/src/Main/main.java
@@ -5,7 +5,7 @@
  */
 package Main;
 
-import Grafo.MetodosGrafo;
+import Grafo.*;
 
 /**
  *
@@ -13,18 +13,35 @@ import Grafo.MetodosGrafo;
  */
 public class main {
     
-    static MetodosGrafo mg = new MetodosGrafo();
+   
+    
     public static void main(String[] args) {
         
-        medicionGrafo();    
+        GraphWeighted graphWeighted = new GraphWeighted(true);
         
+        NodeWeighted zero = new NodeWeighted(0);
+        NodeWeighted one = new NodeWeighted(1);
+        NodeWeighted two = new NodeWeighted(2);
+        NodeWeighted three = new NodeWeighted(3);
+        NodeWeighted four = new NodeWeighted(4);
+        NodeWeighted five = new NodeWeighted(5);
+        NodeWeighted six = new NodeWeighted(6);
+
+        graphWeighted.addEdge(zero, one, 8);
+        graphWeighted.addEdge(zero, two, 11);
+        graphWeighted.addEdge(one, three, 3);
+        graphWeighted.addEdge(one, four, 8);
+        graphWeighted.addEdge(one, two, 7);
+        graphWeighted.addEdge(two, four, 9);
+        graphWeighted.addEdge(three, four, 5);
+        graphWeighted.addEdge(three, five, 2);
+        graphWeighted.addEdge(four, six, 6);
+        graphWeighted.addEdge(five, four, 1);
+        graphWeighted.addEdge(five, six, 8);
+
+        graphWeighted.DijkstraShortestPath(zero, six);
         
     }
     
-    static void medicionGrafo() {//medicion de los datos del grafos
-        System.out.println("------------Grafo--------------");
-         //Datos de entrada
-        mg.insertAutomatico(5); 
-    }
     
 }
--- Proyecto2Analisis/src/Test/MetodosGrafo.java
+++ Proyecto2Analisis/src/Test/MetodosGrafo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Grafo;
+package Test;
 
 /**
  *
