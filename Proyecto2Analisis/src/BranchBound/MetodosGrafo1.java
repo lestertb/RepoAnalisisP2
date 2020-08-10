5f23094f5dc8c1f6a118883792748309a360aacf
Marco <marcoespinozasalas@gmail.com>
2020-08-09 20:45:27 -0600

    Final
    Muestreo

--- Proyecto2Analisis/src/BranchBound/MetodosGrafo1.java
+++ Proyecto2Analisis/src/BranchBound/MetodosGrafo1.java
@@ -1,6 +1,7 @@
 //package
 package BranchBound;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +14,34 @@ import java.util.Map;
 public class MetodosGrafo1 {
 
  public static  Vertice1 grafo;
+ 
+ public static double memoriaGenetico;
+ public static double memoriaBacktraking;
+ public static double memoriaRamiyPoda;
+ 
+  //Genetico
+    public static int comparacionesGenetico;
+    public static int asignacionesGenetico;
+    public static int LineasGenetico;
+     public static double FinGenetico = 0;
+    public static double InicioGenetico = 0;
+    public static double tiempoGenetico = 0;
+    
+    //RamiyPoda
+     public static int comparacionesRamiyPoda;
+    public static int asignacionesRamiyPoda;
+    public static int LineasRamiyPoda;
+     public static double FinRamiyPoda = 0;
+    public static double InicioRamiyPoda = 0;
+    public static double tiempoRamiyPoda = 0;
+    
+     //Backtraking
+    public static int comparacionesBack;
+    public static int asignacionesBack;
+    public static int LineasBack;
+     public static double FinBack = 0;
+    public static double InicioBack = 0;
+    public static double tiempoBack = 0;
 
  public static MetodosGrafo1 instance = null;
     public static MetodosGrafo1 getInstance(){
@@ -144,32 +173,61 @@ public class MetodosGrafo1 {
     
     
     
-    ArrayList<Vertice1> genetico(Vertice1 grafo, int cant,int poblacion)
-    { 
+    ArrayList<Vertice1> genetico(Vertice1 grafo, int cant,int poblacion){      
         ArrayList<Vertice1> Generacion = new ArrayList<>();
+         asignacionesGenetico++;
+         LineasGenetico++;
         Generacion.add(grafo);
         for (int i = 0; i < poblacion-2; i++) {
+            comparacionesGenetico++;
+            LineasGenetico++;
             int num = (int) (Math.random() * cant-1) + 1;
+            memoriaGenetico+=32;
+            asignacionesGenetico++;
+            LineasGenetico++;
             Vertice1 n = buscar(num);
+            asignacionesGenetico++;
+            LineasGenetico++;
             boolean k= true;
+            memoriaGenetico+=8;
+            asignacionesGenetico++;
+            LineasGenetico++;
             while(k==true)
             {
+            comparacionesGenetico++;
+            LineasGenetico++;
                 num = (int) (Math.random() * cant-1) + 1;
+                asignacionesGenetico++;
+                LineasGenetico++;
                 n = buscar(num);
+                asignacionesGenetico++;
+                LineasGenetico++;
                 k=false;
+                asignacionesGenetico++;
+                LineasGenetico++;
                 for (Vertice1 x: Generacion)
                 {
+                    comparacionesGenetico++;
+                    LineasGenetico++;
                     if ((x.num==num)|(num==(cant-1))) {
+                        comparacionesGenetico++;
+                        LineasGenetico++;
                         k=true;
+                        asignacionesGenetico++;
+                        LineasGenetico++;
                     }
                 }  
             }
             Generacion.add(n);
+            LineasGenetico++;
         }
         Generacion.add(buscar(cant-1));
+        LineasGenetico++;
         //System.out.println(Generacion.size());
         for(Vertice1 t1 :Generacion)
         {
+            comparacionesGenetico++;
+            LineasGenetico++;
             System.out.print(t1.num+" - ");
             
             
@@ -179,104 +237,186 @@ public class MetodosGrafo1 {
         return Generacion;
      }
     
-    public void cruceGenetico(Vertice1 grafo, int cant,int poblacion)
-    { 
+    public void cruceGenetico(Vertice1 grafo, int cant,int poblacion){ 
         int max=0;
+        memoriaGenetico+=32;
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1> padre = genetico(grafo, cant,poblacion);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1> madre = genetico(grafo, cant,poblacion);
+        asignacionesGenetico++;
+        LineasGenetico++;
         
         Map<Integer, ArrayList<Vertice1> > lists = apareo(padre, madre,cant);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l1 = lists.get(1);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l2 = lists.get(2);
+        asignacionesGenetico++;
+        LineasGenetico++;
 
         Map<Integer, ArrayList<Vertice1> > lists123 = apareo(l1, l2,cant);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l3 = lists123.get(1);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l4 = lists123.get(2);
-        
+        asignacionesGenetico++;
+        LineasGenetico++;
         Map<Integer, ArrayList<Vertice1> > lists2 = apareo(l3,l4 ,cant);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l5 = lists2.get(1);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l6 = lists2.get(2);
+        asignacionesGenetico++;
+        LineasGenetico++;
         
         Map<Integer, ArrayList<Vertice1> > lists3 = apareo(l5, l6,cant);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l7 = lists3.get(1);
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1>  l8 = lists3.get(2);
+        asignacionesGenetico++;
+        LineasGenetico++;
         
         Map<Integer, ArrayList<Vertice1> > lists4 = apareo(l7, l8,cant);
-
+        asignacionesGenetico++;
+        LineasGenetico++;
+        System.out.println("");
+        System.out.println("Asignaciones"+" "+ asignacionesGenetico);
+        System.out.println("Comparaciones"+" "+ comparacionesGenetico);
+        System.out.println("Lineas Genetico"+" "+ LineasGenetico);
+        System.out.println("Memoria"+" "+ memoriaGenetico);
         
     }
     
-    public Map<Integer, ArrayList<Vertice1>> apareo(ArrayList<Vertice1> padre, ArrayList<Vertice1> madre,int cant)
-    {
+    public Map<Integer, ArrayList<Vertice1>> apareo(ArrayList<Vertice1> padre, ArrayList<Vertice1> madre,int cant){
         Map<Integer, ArrayList<Vertice1> > listMap = new HashMap<Integer, ArrayList<Vertice1> >();
+        asignacionesGenetico++;
+        LineasGenetico++;
         
         int co = sacarComun(padre,madre,cant);
+        memoriaGenetico+=32;
+        asignacionesGenetico++;
+        LineasGenetico++;
         ArrayList<Vertice1> reg = new ArrayList<>();
+        asignacionesGenetico++;
+        LineasGenetico++;
         for(Vertice1 t1 :padre){
+            comparacionesGenetico++;
+            LineasGenetico++;
             if(!reg.contains(t1)){
+                comparacionesGenetico++;
+                LineasGenetico++;
                     reg.add(t1);
+                    LineasGenetico++;
                }
             
 
             if(padre.indexOf(t1)==co)
             {
+                comparacionesGenetico++;
+                LineasGenetico++;
                 break;
             }
         }
         for(Vertice1 t2 :madre){
-            
+            comparacionesGenetico++;
+            LineasGenetico++;
             if(madre.indexOf(t2)>co)
             {
+                comparacionesGenetico++;
+                LineasGenetico++;
                 if(!reg.contains(t2)){
+                    comparacionesGenetico++;
+                    LineasGenetico++;
                     reg.add(t2);
+                    LineasGenetico++;
                }
             
             }
         }
         
         ArrayList<Vertice1> reg2 = new ArrayList<>();
+        asignacionesGenetico++;
+        LineasGenetico++;
         for(Vertice1 t3 :madre){
+            comparacionesGenetico++;
+            LineasGenetico++;
             if(!reg2.contains(t3)){
+                comparacionesGenetico++;
+                LineasGenetico++;
                     reg2.add(t3);
+                     LineasGenetico++;
                }
             
             if(madre.indexOf(t3)==co){
+                comparacionesGenetico++;
+                LineasGenetico++;
                 break;
             }
         }
         for(Vertice1 t4 :padre){
+            comparacionesGenetico++;
+                LineasGenetico++;
             if(padre.indexOf(t4)>co)
             {
+                comparacionesGenetico++;
+                LineasGenetico++;
                 if(!reg2.contains(t4)){
+                comparacionesGenetico++;
+                LineasGenetico++;
                     reg2.add(t4);
+                    LineasGenetico++;
                }
             
             }
         }
         System.out.println("--------chamaco 1");
         for(Vertice1 t :reg){
+            comparacionesGenetico++;
+            LineasGenetico++;
             System.out.print(t.num+"-");
         }
         System.out.println("\n--------chamaco 2");
         for(Vertice1 t1 :reg2){
+            comparacionesGenetico++;
+            LineasGenetico++;
             System.out.print(t1.num+"-");
         }
+        LineasGenetico++;
         Generaciones.add(reg);
+        LineasGenetico++;
         Generaciones.add(reg2);
         
         listMap.put(1, reg);
+        LineasGenetico++;
         listMap.put(2, reg2);
+        LineasGenetico++;
         return listMap;
 
     }
     
     
-    int sacarComun(ArrayList<Vertice1> padre, ArrayList<Vertice1> madre,int cant)
-    { 
+    int sacarComun(ArrayList<Vertice1> padre, ArrayList<Vertice1> madre,int cant){ 
         for(Vertice1 pa :padre){
+            comparacionesGenetico++;
+            LineasGenetico++;
             for(Vertice1 ma :madre){
+                comparacionesGenetico++;
+                LineasGenetico++;
                 if((pa.equals(ma))&(pa.num!=0)&(pa.num!=cant-1)){
-                    
+                    comparacionesGenetico++;
+                    LineasGenetico++;
                     System.out.println("\nnumero de la suerte: "+pa.num);
                     return padre.indexOf(pa);
                 }
@@ -312,32 +452,69 @@ public class MetodosGrafo1 {
     String rutaMenor = "";
     int menorValor = 0;
     public void backtracking(Vertice1 origen, Vertice1 destino, String ruta, int dist) {
+        memoriaBacktraking+=32;
+        comparacionesBack++;
+        LineasBack++;
         if ((origen == null) || (origen.visitado == true)) {
+            LineasBack++;
             return;
         }
         if (origen == destino) {
+            comparacionesBack++;
+            LineasRamiyPoda++;
             System.out.println("Ruta: " + ruta + destino.num);
             System.out.println("Con una distancia de: " + dist);
 
             if (("".equals(rutaMenor)) || (menorValor > dist)) {
+                comparacionesBack++;
+                LineasBack++;
                 rutaMenor = ruta + destino.num;
+                
+                asignacionesBack++;
+                LineasBack++;
                 menorValor = dist;
+                memoriaBacktraking+= 32;
+                 asignacionesBack++;
+                LineasBack++;
             }
             existe = true;
+            memoriaBacktraking+= 8;
+            asignacionesBack++;
+            LineasBack++;
             return;
         }
         origen.visitado = true;
+        memoriaBacktraking+= 8;
+        asignacionesBack++;
+        LineasBack++;
         Arco1 a = origen.subListaArcos;
+        asignacionesBack++;
+        LineasBack++;
         while (a != null) {
+            LineasBack++;
+            comparacionesBack++;
+            asignacionesBack++;
             backtracking(a.destino, destino, (ruta + (origen.num)).toString(), dist + a.size);
             a = a.sigA;
+            LineasBack++;
+            asignacionesBack++;
         }
         origen.visitado = false;
+        memoriaBacktraking+= 8;
+        LineasBack++;
+        asignacionesBack++;
     }
-    public void var()
+    public void var() throws UnsupportedEncodingException
     {
         System.out.println(rutaMenor);
+        byte[] b = rutaMenor.getBytes("UTF-8");
+        System.out.println(b.length+"Hola");
         System.out.println(menorValor);
+        System.out.println("");
+        System.out.println("Asignaciones"+" "+ asignacionesBack);
+        System.out.println("Comparaciones"+" "+ comparacionesBack);
+        System.out.println("Lineas backtracking"+" "+ LineasBack);
+        System.out.println("Memoria backtracking"+" "+ memoriaBacktraking);
     }
     
     
@@ -345,51 +522,94 @@ public class MetodosGrafo1 {
     int menorValor2 = 0;
     int max =0;
     public void ramiPoda(Vertice1 origen, Vertice1 destino, String ruta, int dist) {
+        memoriaRamiyPoda+= 32;
+        memoriaRamiyPoda+= 32;
+        comparacionesRamiyPoda++;
+        LineasRamiyPoda++;
         if ((origen == null) || (origen.visitado == true)) {
+            LineasRamiyPoda++;
             return;
         }
         
         if (origen == destino) {
+            comparacionesRamiyPoda++;
+            LineasRamiyPoda++;
             System.out.println("Ruta: " + ruta + destino.num);
             System.out.println("Con una distancia de: " + dist);
 
             if (("".equals(rutaMenor2)) || (menorValor2 > dist)) {
+                comparacionesRamiyPoda++;
+                LineasRamiyPoda++;
                 rutaMenor2 = ruta + destino.num;
+                
+                asignacionesRamiyPoda++;
+                LineasRamiyPoda++;
                 menorValor2 = dist;
+                memoriaRamiyPoda+= 32;
+                asignacionesRamiyPoda++;
+                LineasRamiyPoda++;
             }
             //existe = true;
             return;
         } 
         if(dist > menorValor2){
+            comparacionesRamiyPoda++;
+            LineasRamiyPoda++;
             if(max<5){
+                LineasRamiyPoda++;
+                comparacionesRamiyPoda++;
                 for(String s :podas){
+                    LineasRamiyPoda++;
+                    comparacionesRamiyPoda++;
                     if(s.equals(ruta)){
-                        
+                        LineasRamiyPoda++;
+                        comparacionesRamiyPoda++;
                         return;
                     }
                 }
                 System.out.println(podas);
                 podas.add(ruta);
+                LineasRamiyPoda++;
                 max++;
             }
             return;
         }
+        asignacionesRamiyPoda++;
+        LineasRamiyPoda++;
         origen.visitado = true;
+        memoriaRamiyPoda+= 8;
+         asignacionesRamiyPoda++;
+        LineasRamiyPoda++;
         Arco1 a = origen.subListaArcos;
+         asignacionesRamiyPoda++;
+        LineasRamiyPoda++;
         while (a != null) {
-
+             comparacionesRamiyPoda++;
+             LineasRamiyPoda++;
+             asignacionesRamiyPoda++;
             ramiPoda(a.destino, destino, (ruta + (origen.num)).toString(), dist + a.size);
+            LineasRamiyPoda++;
+             asignacionesRamiyPoda++;
             a = a.sigA;
         }
+        LineasRamiyPoda++;
+        asignacionesRamiyPoda++;
         origen.visitado = false;
+        memoriaRamiyPoda+= 8;
     }
     
-    public void var2()
+    public void var2() throws UnsupportedEncodingException
     {
         
         System.out.println(rutaMenor2);
+         byte[] b = rutaMenor2.getBytes("UTF-8");
+        System.out.println(b.length+"Hola");
         System.out.println(menorValor2);
         System.out.println(podas);
+        System.out.println("Asignaciones"+" "+ asignacionesRamiyPoda);
+        System.out.println("Comparaciones"+" "+ comparacionesRamiyPoda);
+        System.out.println("Lineas Ramificacion y Poda"+" "+ LineasRamiyPoda);
+        System.out.println("Memoria"+ " " + memoriaRamiyPoda);
         
     }
     
--- Proyecto2Analisis/src/Grafo/MetodosGrafo.java
+++ Proyecto2Analisis/src/Grafo/MetodosGrafo.java
@@ -5,6 +5,7 @@
  */
 package Grafo;
 
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 /**
@@ -16,6 +17,25 @@ public class MetodosGrafo {
     //Globals
     public Set<Vertice> arcos;
     public boolean directed;
+    public static double memoriaVoraz;
+    public static double memoriaDinamico;
+    //Voraz
+    public static int comparacionesVoraz;
+    public static int asignacionesVoraz;
+    public static int LineasVoraz;
+     public static double FinVoraz = 0;
+    public static double InicioVoraz = 0;
+    public static double tiempoVoraz = 0;
+    
+    //PrograDinamica
+    public static int comparacionesDinamica;
+    public static int asignacionesDinamica;
+    public static int LineasDinamica;
+    public static double FinDinamica = 0;
+    public static double InicioDinamica = 0;
+    public static double tiempoDinamica = 0;
+    
+  
 
     //Globals para el génetico
     public static ArrayList<Integer> hechos = new ArrayList<>();
@@ -106,85 +126,154 @@ public class MetodosGrafo {
     //Voraz
     public void voraz(Vertice origen, Vertice destino, int tamaño) {
         Stack rutaVertice = new Stack();
+        LineasVoraz++;
+        asignacionesVoraz++;
         rutaVertice.push(origen);
+         LineasVoraz++;
         while (!rutaVertice.isEmpty()) {
+            asignacionesVoraz++;
+            comparacionesVoraz++;
             Vertice vertice = (Vertice) rutaVertice.pop();
+            LineasVoraz++;
+            asignacionesVoraz++;
             double pesoActual = Double.MAX_VALUE;
+            memoriaVoraz+=64;
+            LineasVoraz++;
+            asignacionesVoraz++;
             Vertice aux = vertice;
+            LineasVoraz++;
+            asignacionesVoraz++;
             if (vertice != destino) {
+                comparacionesVoraz++;
                 if (!vertice.isVisited()) {
+                    comparacionesVoraz++;
                     vertice.visited = true;
-                    for (Arco arco : vertice.arcos) {
+                    memoriaVoraz+=8;
+                    LineasVoraz++;
+                    asignacionesVoraz++;
+                    for (Arco arco : vertice.arcos)
+                    {
+                        comparacionesVoraz++;
                         if (arco.peso < pesoActual) {
-
+                            comparacionesVoraz++;
                             if (arco.destino.id > vertice.id) {
+                                comparacionesVoraz++;
                                 pesoActual = arco.peso;
+                                LineasVoraz++;
+                                asignacionesVoraz++;
                                 aux = arco.destino;
+                                LineasVoraz++;
+                                asignacionesVoraz++;
                             }
                         }
                     }
                     rutaVertice.remove(vertice);
+                    LineasVoraz++;
                     rutaVertice.push(aux);
+                    LineasVoraz++;
                     System.out.println("Origen:" + vertice.id);
                     System.out.println("Destino:" + aux.id);
                 }
             }
         }
+        System.out.println("Asignaciones"+" "+ asignacionesVoraz);
+        System.out.println("Comparaciones"+" "+ comparacionesVoraz);
+        System.out.println("Lineas voraz"+" "+ LineasVoraz);
+        System.out.println("Memoria en Voraz"+" "+ memoriaVoraz);
 
     }
 
     //Progra Dinamica
-    public void DijkstraShortestPath(Vertice inicio, Vertice fin) {
-
+    public void DijkstraShortestPath(Vertice inicio, Vertice fin) throws UnsupportedEncodingException {
+        
         HashMap<Vertice, Vertice> changedAt = new HashMap<>();
+        asignacionesDinamica++;
+        LineasDinamica++;
         changedAt.put(inicio, null);
-
+        LineasDinamica++;
         // Realiza un seguimiento de la ruta más corta que hemos encontrado hasta ahora para cada vertice
         HashMap<Vertice, Double> shortestPathMap = new HashMap<>();
+        asignacionesDinamica++;
+        LineasDinamica++;
 
         // Establecer el peso de la ruta más corta de cada vertice en infinito positivo para inicio
         // excepto el nodo inicial, cuyo peso de ruta más corto es 0
         for (Vertice node : arcos) {
+            comparacionesDinamica++;
+            LineasDinamica++;
             if (node == inicio) {
+            comparacionesDinamica++;
+            LineasDinamica++;
                 shortestPathMap.put(inicio, 0.0);
+            LineasDinamica++;
             } else {
                 shortestPathMap.put(node, Double.POSITIVE_INFINITY);
+                LineasDinamica++;
             }
         }
 
         // Ahora pasamos por todos los vértices a los que podemos ir desde el nodo inicial
         // (esto mantiene el ciclo un poco más simple)
         for (Arco edge : inicio.arcos) {
+            comparacionesDinamica++;
+            LineasDinamica++;
             shortestPathMap.put(edge.destino, edge.peso);
+            LineasDinamica++;
             changedAt.put(edge.destino, inicio);
+            LineasDinamica++;
         }
 
         inicio.visit();
+        LineasDinamica++;
 
         // Este bucle se ejecuta siempre que haya un nodo no visitado que podamos
         // llegar desde cualquiera de los vértices que pudimos hasta entonces
         while (true) {
+            comparacionesDinamica++;
+            LineasDinamica++;
             Vertice currentNode = masCercanoAccesibleUnvisited(shortestPathMap);
+            asignacionesDinamica++;
+            LineasDinamica++;
             // Si aún no hemos llegado al nodo fin y no hay otro
             // vertice alcanzable la ruta entre inicio y fin no existe
             // (no están conectados)
             if (currentNode == null) {
+                comparacionesDinamica++;
+                LineasDinamica++;
                 System.out.println("No hay un ruta entre " + inicio.id + " y " + fin.id);
                 return;
             }
 
             // Si el nodo no visitado más cercano es nuestro destino, queremos imprimir la ruta
             if (currentNode == fin) {
+                comparacionesDinamica++;
+                LineasDinamica++;
                 System.out.println("El camino con el menor peso entre "
                         + inicio.id + " y " + fin.id + " es:");
 
                 Vertice hijo = fin;
+                asignacionesDinamica++;
+                LineasDinamica++;
                 //Se convierte en string
                 String str1 = Integer.toString(fin.id);
+                byte[] b = str1.getBytes("UTF-8");
+                System.out.println(b.length+"Hola");
+                asignacionesDinamica++;
+                LineasDinamica++;
                 String path = str1;
+                 byte[] p = path.getBytes("UTF-8");
+                 System.out.println(p.length+"Hola");
+                asignacionesDinamica++;
+                LineasDinamica++;
                 while (true) {
+                comparacionesDinamica++;
+                LineasDinamica++;
                     Vertice padre = changedAt.get(hijo);
+                    asignacionesDinamica++;
+                    LineasDinamica++;
                     if (padre == null) {
+                         comparacionesDinamica++;
+                         LineasDinamica++;
                         break;
                     }
 
@@ -192,10 +281,18 @@ public class MetodosGrafo {
                     // para imprimir la ruta necesitamos agregar el padre antes que el hijo y
                     // son descendientes
                     path = padre.id + " " + path;
+                    asignacionesDinamica++;
+                    LineasDinamica++;
                     hijo = padre;
+                    asignacionesDinamica++;
+                    LineasDinamica++;
                 }
                 System.out.println(path);
                 System.out.println("Peso total ruta minima: " + shortestPathMap.get(fin));
+                System.out.println("Asignaciones"+" "+ asignacionesDinamica);
+                System.out.println("Comparaciones"+" "+ comparacionesDinamica);
+                System.out.println("Lineas dinamico"+" "+ LineasDinamica);
+                System.out.println("Memoria"+" "+ memoriaDinamico);
                 return;
             }
             currentNode.visit();
@@ -204,13 +301,19 @@ public class MetodosGrafo {
             // y compruebe si su valor de ruta más corto es mejor al pasar por nuestro
             // vertice actual que el que teníamos antes
             for (Arco edge : currentNode.arcos) {
+                 comparacionesDinamica++;
+                    LineasDinamica++;
                 if (edge.destino.isVisited()) {
+                    comparacionesDinamica++;
+                    LineasDinamica++;
                     continue;
                 }
 
                 if (shortestPathMap.get(currentNode)
                         + edge.peso
                         < shortestPathMap.get(edge.destino)) {
+                    comparacionesDinamica++;
+                    LineasDinamica++;
                     shortestPathMap.put(edge.destino,
                             shortestPathMap.get(currentNode) + edge.peso);
                     changedAt.put(edge.destino, currentNode);
@@ -222,20 +325,41 @@ public class MetodosGrafo {
     private Vertice masCercanoAccesibleUnvisited(HashMap<Vertice, Double> shortestPathMap) {
 
         double menorDistancia = Double.POSITIVE_INFINITY;
+        memoriaDinamico+=64;
+        asignacionesDinamica++;
+        LineasDinamica++;
         Vertice verticeAccesibleMasCercano = null;
+        memoriaDinamico+=8;
+        asignacionesDinamica++;
+        LineasDinamica++;
         for (Vertice node : arcos) {
+            comparacionesDinamica++;
+            LineasDinamica++;
             if (node.isVisited()) {
+            comparacionesDinamica++;
+            LineasDinamica++;
                 continue;
             }
 
             double distanciaActual = shortestPathMap.get(node);
+            memoriaDinamico+=64;
+            asignacionesDinamica++;
+            LineasDinamica++;
             if (distanciaActual == Double.POSITIVE_INFINITY) {
+                comparacionesDinamica++;
+                LineasDinamica++;
                 continue;
             }
 
             if (distanciaActual < menorDistancia) {
+                comparacionesDinamica++;
+                LineasDinamica++;
                 menorDistancia = distanciaActual;
+                asignacionesDinamica++;
+                LineasDinamica++;
                 verticeAccesibleMasCercano = node;
+                asignacionesDinamica++;
+                LineasDinamica++;
             }
         }
         return verticeAccesibleMasCercano;
